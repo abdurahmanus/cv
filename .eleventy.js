a244bffb7356915cf1ef9b6c7cfbc6a2447d2bdc
Dmitry Gladkikh <abdurahmanus@gmail.com>
2022-03-16 15:53:08 +0300

    try to add image

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,31 @@
+const Image = require("@11ty/eleventy-img");
+
+async function imageShortcode(src, alt, sizes) {
+  let metadata = await Image(src, {
+    widths: [300, 600],
+    formats: ["avif", "jpeg"],
+    outputDir: "./_site/img/",
+    urlPath: "img/"
+  });
+
+  let imageAttributes = {
+    alt,
+    sizes,
+    loading: "lazy",
+    decoding: "async",
+  };
+
+  // You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
+  return Image.generateHTML(metadata, imageAttributes);
+}
+
 module.exports = (eleventyConfig) => {
+  eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
   eleventyConfig.addPassthroughCopy("src/css");
   return {
     passthroughFileCopy: true,
     dir: {
       input: "src",
-      output: "docs",
     },
   };
 };
