b56cbabd3c48818e29debaaaf10d54f6cf7e073c
Dmitry Gladkikh <abdurahmanus@gmail.com>
2022-03-16 20:23:53 +0300

    styling

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,14 @@
+const path = require("path");
 const Image = require("@11ty/eleventy-img");
 
-async function imageShortcode(src, alt, sizes) {
-  let metadata = await Image(src, {
-    widths: [1280, 600, 300, null],
+async function imageShortcode(src, cls, alt, sizes) {
+  const source = path.join(__dirname, "src/_includes/", src);
+
+  let metadata = await Image(source, {
+    widths: [300, 600],
     formats: ["avif", "jpeg"],
     outputDir: "./_site/img/",
-    urlPath: "img/"
+    urlPath: "img/",
   });
 
   let imageAttributes = {
@@ -13,14 +16,49 @@ async function imageShortcode(src, alt, sizes) {
     sizes,
     loading: "lazy",
     decoding: "async",
+    class: cls,
   };
 
   // You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
   return Image.generateHTML(metadata, imageAttributes);
 }
 
+function generateImages(src, widths) {
+  let source = path.join(__dirname, "src/_includes/", src);
+  let options = {
+    widths,
+    formats: ["avif", "jpeg"],
+    outputDir: "./_site/img",
+    urlPath: "img/",
+    useCache: true,
+    sharpJpegOptions: {
+      quality: 99,
+      progressive: true,
+    },
+  };
+  // genrate images, ! dont wait
+  Image(source, options);
+  // get metadata even the image are not fully generated
+  return Image.statsSync(source, options);
+}
+
+function imageCssBackground(src, selector, widths) {
+  const metadata = generateImages(src, widths);
+  let markup = [
+    `${selector} { background-image: url(${metadata.jpeg[0].url});} `,
+  ];
+  // i use always jpeg for backgrounds
+  metadata.jpeg.slice(1).forEach((image, idx) => {
+    markup.push(
+      `@media (min-width: ${metadata.jpeg[idx].width}px) { ${selector} {background-image: url(${image.url});}}`
+    );
+  });
+  return markup.join("");
+}
+
 module.exports = (eleventyConfig) => {
   eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
+  eleventyConfig.addNunjucksShortcode("cssBackground", imageCssBackground);
   eleventyConfig.addPassthroughCopy("src/css");
   return {
     passthroughFileCopy: true,
--- src/index.md
+++ src/index.md
@@ -2,8 +2,12 @@
 layout: layout.njk
 ---
 
+<section>
+
 # About
 
 I am an experienced front-end developer who enjoys building modern UI using the latest in web technologies. I frequently leverage the React and Redux ecosystems (@redux-toolkit). I love styled-components, xstyled, tailwindcss.
 
 Currently, I work at Kaspersky Lab on the Infrastructure Solutions & Services Development team.
+
+</section>
